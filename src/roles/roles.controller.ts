3203b10c7bcd859f57b37381f3fb6af592f680e4
Julia-Dubenchuk <dubenchuk6@gmail.com>
2026-03-18 19:35:03 +0200

    Task-44: Add Authentication Guards to findAll() and findOne() Endpoints in RolesController

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -6,12 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { ActionOnResource } from '../auth/decorators/action-on-resource.decorator';
 import { RoleName } from './entities/role.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('roles')
 export class RolesController {
@@ -24,11 +26,13 @@ export class RolesController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.rolesService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.rolesService.findOne(id);
   }
