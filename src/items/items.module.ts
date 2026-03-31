4ca09c52926a0f524d66c844d676726d970501c8
Julia-Dubenchuk <dubenchuk6@gmail.com>
2026-03-31 18:42:24 +0300

    Refactoring: remove ItemsAdminController from e2e test

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
-import { ItemsAdminController } from './controllers/items-admin.controller';
 import { Item } from './entities/item.entity';
 import { AuthModule } from '../auth/auth.module';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -10,7 +9,7 @@ import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Item, User]), AuthModule],
-  controllers: [ItemsController, ItemsAdminController],
+  controllers: [ItemsController],
   providers: [ItemsService, RolesGuard],
 })
 export class ItemsModule {}
